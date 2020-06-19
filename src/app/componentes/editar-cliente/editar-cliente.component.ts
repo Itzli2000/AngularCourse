d109a9a61ccddec0c06ed18f67f1f877b5786670
Ariel  Molina <itzli2000@msn.com>
2020-06-18 20:32:41 -0500

    Feature Added functions to edit/delete client

--- src/app/componentes/editar-cliente/editar-cliente.component.ts
+++ src/app/componentes/editar-cliente/editar-cliente.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteServicio } from 'src/app/servicios/cliente.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Cliente } from 'src/app/modelo/cliente.model';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-editar-cliente',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarClienteComponent implements OnInit {
 
-  constructor() { }
+  cliente: Cliente = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0
+  };
+
+  id: string;
+
+  constructor(private clientesServicio: ClienteServicio,
+              private flashMessages: FlashMessagesService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const paramId = 'id';
+    this.id = this.route.snapshot.params[paramId];
+    this.clientesServicio.getCliente(this.id).subscribe(cliente => {
+      this.cliente = cliente;
+    });
+  }
+
+  guardar({ value, valid }: { value: Cliente, valid: boolean }) {
+    if (!valid) {
+      this.flashMessages.show('Por favor llena el formulario correctamente',
+      {
+        cssClass: 'alert-danger',
+        timeout: 4000
+      });
+    } else {
+      value.id = this.id;
+      this.clientesServicio.modificarCliente(value);
+      this.router.navigate(['/']);
+    }
+  }
+
+  eliminar() {
+    if (confirm('¿Seguro que desea eliminar el cliente?')) {
+      this.clientesServicio.eliminarCliente(this.cliente);
+      this.router.navigate(['/'])
+    }
   }
 
 }
